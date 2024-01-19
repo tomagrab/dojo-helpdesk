8577b9ede2f5bfe31d52227b4ff0e32bc9169eb4
Tommy Grabowski <tommy.grabowski@velocitor.com>
2024-01-19 12:06:20 -0500

    Updated API route to have TicketType

--- src/app/api/tickets/route.ts
+++ src/app/api/tickets/route.ts
@@ -1,4 +1,4 @@
-import { Ticket } from '@/lib/Types/Ticket/TicketType';
+import { TicketType } from '@/lib/Types/Ticket/TicketType';
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
@@ -6,7 +6,7 @@ import { NextResponse } from 'next/server';
 export const dynamic = 'force-dynamic';
 
 export async function POST(request: Request) {
-  const ticket: Ticket = await request.json();
+  const ticket: TicketType = await request.json();
 
   // Get Supabase Instance
   const supabase = createRouteHandlerClient({
